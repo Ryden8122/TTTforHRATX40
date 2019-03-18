d3b202b508486f8bff28ca4c75c98436f4bd5099
Ryan Ferrer <ryferr8122@gmail.com>
2019-03-18 11:47:36 -0500

    dynamically renders a table from app.js

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 //What if we made a for loop that drew out table for us?
-//Try parentNode.append();
+
+// Draws the giant hashtag - although that is not what it is called
+
+/*
+  Create a table div
+    Create row div who targets table div
+    Create 3 spans that target a row div
+
+  append table to app
+*/
+function drawGrid() {
+  let appendTarget = document.getElementById("app");
+  let tableDiv = document.createElement("div");
+  tableDiv.className = "table";
+  
+  for (let i = 0; i < 3; i++){
+    let rowDiv = document.createElement("div");
+    rowDiv.className = "row" + i;
+
+    for (let j = 0; j < 3; j++){
+      let cellSpan = document.createElement("span");
+      cellSpan.className = "cell" + j;
+
+      rowDiv.append(cellSpan);
+    }
+    tableDiv.append(rowDiv);
+  }
+  appendTarget.append(tableDiv);
+}
 
 document.addEventListener("DOMContentLoaded", function(e){
-  let targetDiv = document.getElementById("app");
-  let greetingDiv = document.createElement("div");
-  let newGreeting = document.createTextNode("Greetings from apps.js");
-
-  greetingDiv.appendChild(newGreeting);
-
-  targetDiv.appendChild(greetingDiv);
+  drawGrid();
 })
